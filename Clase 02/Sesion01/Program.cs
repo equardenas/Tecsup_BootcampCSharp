4a4c503031fd362aab2ac2ad4e1f67cba336c320
Edgar Cardenas Navarro <ecardenas.sitired@gmail.com>
2024-09-04 20:44:18 -0500

    Clase 2 C# : variables

--- Clase 02/Sesion01/Program.cs
+++ Clase 02/Sesion01/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hola Alumnos");
+string nombre = "Edgar Cárdenas Navarro";
+int edad = 20;
+long altura = 165;
+double dinero = 1651.36;
+float danio = 124.4f;
+decimal montoEnBanco = 12451651.4m;
+char letra = 'A';
+bool TieneGorro = false;
+var kilometros = 251531.5454;
+
+Console.WriteLine("Escribe tu nombre");
+nombre = Console.ReadLine();
+Console.WriteLine("Mi nombre es es " + nombre + ", mi edad es " + edad);
+
